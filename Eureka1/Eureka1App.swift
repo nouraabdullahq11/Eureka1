1820398c3d43283942c3553cca65443331065306
noura <nouraabdullahahq@gmail.com>
2024-05-16 00:52:09 +0300

    Initial Commit

---
+++ Eureka1/Eureka1App.swift
@@ -0,0 +1,17 @@
+//
+//  Eureka1App.swift
+//  Eureka1
+//
+//  Created by Noura Alqahtani on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Eureka1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
